67f340013ef7d92908418717a42b522afbf1bfec
Francesco Galvão <francescogalvao@gmail.com>
2025-05-02 18:49:20 -0300

    Add initial Spring Boot application with Maven wrapper and configuration files

---
+++ src/main/java/com/pampa/springfinder/SpringfinderApplication.java
@@ -0,0 +1,13 @@
+package com.pampa.springfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringfinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pampa/springfinder/SpringfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pampa.springfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
