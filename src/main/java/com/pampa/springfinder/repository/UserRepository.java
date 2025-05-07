4c98f34d5c2da8c4b4104534db9bf33dc86dd302
Francesco Galvão <francescogalvao@gmail.com>
2025-05-07 03:24:31 +0100

    final corrections

--- src/main/java/com/pampa/springfinder/repository/UserRepository.java
+++ src/main/java/com/pampa/springfinder/repository/UserRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserModel, String> {
 
-    List<UserModel> findFirst1000AllByNameLike(String name);
-    List<UserModel> findFirst1000AllByDateLike(String date);
+    List<UserModel> findFirst1000ByNameLike(String name);
+    List<UserModel> findFirst1000ByDate(String date);
 }
